9f0ce115908e69014443424b9e222dea8a94fbb8
Steve Hobbs <elkdanger@googlemail.com>
2014-04-29 21:09:17 +0100

    Added a pass-through bundle orderer

---
+++ src/Kilo.Mvc/Optimization/PassThruBundleOrderer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Web.Optimization;
+
+namespace Kilo.Mvc.Optimization
+{
+    /// <summary>
+    /// Returns the files in the order that they're added to the bundle, rather than changing the order based on certain keywords.
+    /// </summary>
+    public class PassThruBundleOrderer : IBundleOrderer
+    {
+        /// <summary>
+        /// Returns the files in the same order they're added to the bundle
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="files">The files.</param>
+        /// <returns>A list of bundle files</returns>
+        public IEnumerable<BundleFile> OrderFiles(BundleContext context, IEnumerable<BundleFile> files)
+        {
+            return files;
+        }
+    }
+}
