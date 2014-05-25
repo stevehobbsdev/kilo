64b64b4125e1c8968a5e1cffa1a92845041e1975
Steve Hobbs <elkdanger@googlemail.com>
2014-05-26 00:52:57 +0100

    Added some extensions for verifying redirect results, and temp data

---
+++ src/Kilo.Testing.MVC/ControllerExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kilo.Testing.Mvc
+{
+    public static class ControllerExtensions
+    {
+        public static void HasTempData(this Controller controller, string key, string value)
+        {
+            Assert.IsNotNull(controller.TempData[key], "The TempData key " + key + " was not found");
+            Assert.AreEqual(value, controller.TempData[key], "The value in TempData[" + key + "] does not match '" + value + "'");
+        }
+    }
+
+}
---
+++ src/Kilo.Testing.MVC/RedirectResultExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kilo.Testing.Mvc
+{
+    public static class RedirectResultExtensions
+    {
+        /// <summary>
+        /// Verifies that the result has redirected to the route dictated by the action, controller and area parameters.
+        /// </summary>
+        /// <param name="result">The result to verify.</param>
+        /// <param name="action">The action.</param>
+        /// <param name="controller">The controller.</param>
+        /// <param name="area">The area.</param>
+        public static void VerifyRoute(this RedirectToRouteResult result, string action, string controller = null, string area = null)
+        {
+            Assert.AreEqual(action, result.RouteValues["action"],
+                string.Format("The route action parameter '{0}' does not match '{1}' - '{2}' was found instead", "action", action, result.RouteValues["action"]));
+
+            Assert.AreEqual(controller, result.RouteValues["controller"],
+                string.Format("The route action parameter '{0}' does not match '{1}' - '{2}' was found instead", "controller", controller, result.RouteValues["controller"]));
+
+            Assert.AreEqual(area, result.RouteValues["area"],
+                string.Format("The route action parameter '{0}' does not match '{1}' - '{2}' was found instead", "area", area, result.RouteValues["area"]));
+        }
+    }
+}
