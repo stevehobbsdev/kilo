4fb094b3596627750d964461c827fc9711005fe9
Steve Hobbs <elkdanger@googlemail.com>
2014-06-30 21:14:36 +0100

    Removed redundant using statements, method cleanup

--- src/Kilo.Testing.MVC/ControllerExtensions.cs
+++ src/Kilo.Testing.MVC/ControllerExtensions.cs
@@ -1,20 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kilo.Testing.Mvc
 {
     public static class ControllerExtensions
     {
-        public static void HasTempData(this Controller controller, string key, string value)
+        /// <summary>
+        /// Asserts that the controller has the specified key and value set into temp data
+        /// </summary>
+        /// <param name="controller">The controller to test</param>
+        /// <param name="key">The key</param>
+        /// <param name="value">The value</param>
+        public static void AssertHasTempData(this Controller controller, string key, object value)
         {
             Assert.IsNotNull(controller.TempData[key], "The TempData key " + key + " was not found");
             Assert.AreEqual(value, controller.TempData[key], "The value in TempData[" + key + "] does not match '" + value + "'");
         }
     }
-
 }
